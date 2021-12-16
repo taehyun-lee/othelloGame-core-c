05e562d6abde980c9b03aea0e1ce05e8e13d8c8e
taehyeon_lee <tedxpem@gmail.com>
2021-12-16 20:43:18 +0900

    printMap() 구현 print.c 추가

---
+++ Othello/print.c
@@ -0,0 +1,43 @@
+﻿#include "common.h"
+
+
+void printMap()
+{
+	int i, j, k;
+	int margin_top = 8;
+	int margin_bottom = 9;
+	int margin_left = 17;
+	int margin_right = 18;
+	int base_line = margin_top;
+
+	gotoxy(margin_left, base_line++);
+	printf("┌");
+	for (i = 0; i < MAPSIZE; i++)
+		printf("───────┬");
+	printf("\b┐");
+
+	for (i = 0; i < MAPSIZE; i++) {
+		for (j = 0; j < 3; j++) {
+			gotoxy(margin_left, base_line++);
+			printf("│");
+			for (k = 0; k < MAPSIZE; k++)
+				printf("       │");
+		}
+		gotoxy(margin_left, base_line++);
+		printf("├");
+		for (j = 0; j < MAPSIZE; j++) {
+			printf("───────┼");
+		}
+		printf("\b┤");
+	}
+	gotoxy(margin_left, --base_line);
+	printf("└");
+	for (i = 0; i < MAPSIZE; i++)
+		printf("───────┴");
+	printf("\b┘");
+}
+
+void gotoxy(int _x, int _y) {
+	COORD pos = { _x, _y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
--- Othello/print.h
+++ Othello/print.h
@@ -1,1 +1,39 @@
 ﻿#pragma once
+#define MAPSIZE 8
+
+/**
+* 게임 시작메인메뉴를 출력
+*/
+void printMainmenu();
+
+/**
+* 부가메뉴 출력(제한시간, 흑/백 스코어, 차례, 나가기버튼)
+*/
+void printSubmenu();
+
+/**
+* 오셀로의 판 출력
+*/
+void printMap();
+
+/**
+* 흑/백 돌을 출력
+*/
+void printStone()
+
+/**
+* 게임 종료 메시지 출력
+* @param _playtime : 총 플레이 시간, _p1_score : p1의 돌 개수, _p2_score : p2의 돌 개수
+*/;
+void printEndMsg(int _playtime, int _p1_score, int _p2_score);
+
+/**
+* 게임 저장 여부 질문 출력
+*/
+void printSave();
+
+/**
+* 콘솔 창에서 x,y좌표로 커서 이동
+* @param _x : 이동할 x좌표, _y : 이동할 y좌표
+*/
+void gotoxy(int _x, int _y);
