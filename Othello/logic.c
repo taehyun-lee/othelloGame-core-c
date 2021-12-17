4896c9e47595680664845d9e829fe38e252eff52
taehyeon_lee <tedxpem@gmail.com>
2021-12-17 13:39:24 +0900

    마우스 입력 테스트
    main.c 에 마우스 입력 함수 추가
    마우스 클릭 시 해당 위치에 "*"출력
    추후 pthread.h 설치 및 연동 필요
    thread를 이용하여 limittime과 마우스 입력 동시성 확인 필요

--- Othello/logic.c
+++ Othello/logic.c
@@ -3,21 +3,22 @@
 
 INPUT_RECORD rec;
 DWORD dwNOER;
-HANDLE CIN = 0;
+//HANDLE CIN = 0;
 
 boolean isClick(time_t _start) {
-    while (1)
-    {
-        time_t end = clock();
-        if ((float)(end - _start) > 30.0)
-            return ;        
-        ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &rec, 1, &dwNOER); // 콘솔창 입력을 받아들임.
-        if (rec.EventType == MOUSE_EVENT) {// 마우스 이벤트일 경우
+	//while (1)
+	//{
+	//	time_t end = clock();
+	//	if ((float)(end - _start) > 30.0)
+	//		return;
+	//	ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &rec, 1, &dwNOER); // 콘솔창 입력을 받아들임.
+	//	if (rec.EventType == MOUSE_EVENT) {// 마우스 이벤트일 경우
 
-            if (rec.Event.MouseEvent.dwButtonState & FROM_LEFT_1ST_BUTTON_PRESSED) { // 좌측 버튼이 클릭되었을 경우
-                return 1;
-                break;
-                }
-            }
-        }
-    }
+	//		if (rec.Event.MouseEvent.dwButtonState & FROM_LEFT_1ST_BUTTON_PRESSED) { // 좌측 버튼이 클릭되었을 경우
+	//			return 1;
+	//			break;
+	//		}
+	//	}
+	//}
+	return 0;
+}
--- Othello/main.c
+++ Othello/main.c
@@ -1,13 +1,93 @@
 ﻿#include "common.h"
-
+#include <pthread.h>
 int map[10][10];
+HANDLE COUT = 0;    // 콘솔 출력 장치
+HANDLE CIN = 0;        // 콘솔 입력 장치
 
-int main() {
-	
-	init();
+int be_input()
+{
+    INPUT_RECORD input_record;
+    DWORD input_count;
 
-	printMainmenu();
-
-	printf("hello");
-	return 0;
+    PeekConsoleInput(CIN, &input_record, 1, &input_count);
+    return input_count;
 }
+
+int get_input(WORD* vkey, COORD* pos)
+{
+    INPUT_RECORD input_record;
+    DWORD input_count;
+
+    ReadConsoleInput(CIN, &input_record, 1, &input_count);
+    switch (input_record.EventType) {
+    case MOUSE_EVENT:
+        if (pos && (input_record.Event.MouseEvent.dwButtonState & FROM_LEFT_1ST_BUTTON_PRESSED)) {
+            CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+            GetConsoleScreenBufferInfo(COUT, &csbi);
+
+            *pos = input_record.Event.MouseEvent.dwMousePosition;
+            pos->X -= csbi.srWindow.Left;
+            pos->Y -= csbi.srWindow.Top;
+
+            return MOUSE_EVENT;
+        }
+        break;
+
+    }
+
+    //    FlushConsoleInputBuffer(CIN);
+    return 0;
+}
+
+void* limit_time_func(void* data) {
+    static int retval = 99;
+    int count = *((int*)data);
+    int i = 0;
+    int start_clk = clock();
+    while (1) {
+        if (clock() - start_clk == 10) {
+            pthread_ex
+        }
+    }
+}
+void main()
+{
+    DWORD mode;
+    WORD key;
+    COORD pos;
+
+    int event;        // 마우스 이벤트에 이용
+    int x;            // 마우스의 x좌표 저장소
+    int y;            // 마우스의 y좌표 저장소
+
+    CIN = GetStdHandle(STD_INPUT_HANDLE);
+    COUT = GetStdHandle(STD_OUTPUT_HANDLE);
+
+
+
+
+
+    // 마우스 활성화
+    GetConsoleMode(CIN, &mode);
+    SetConsoleMode(CIN, mode | ENABLE_MOUSE_INPUT);
+    int clk_s = clock();
+    while (1)
+    {
+        if (be_input())
+        {
+            if (get_input(&key, &pos) != 0)
+            {
+                MOUSE_EVENT;
+                x = pos.X;    // 마우스클릭값이 x,y변수에 저장되도록함
+                y = pos.Y;
+                gotoxy(x, y);
+                printf("*");
+            }
+        }
+
+    }
+}
+
+
+//출처: https://www.crocus.co.kr/4 [Crocus]
