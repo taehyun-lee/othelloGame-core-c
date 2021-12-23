1c63421832ba915853e6c3def5a4a80739c0ef3b
taehyeon_lee <tedxpem@gmail.com>
2021-12-23 19:11:40 +0900

    init() 돌 초기화 추가, backup.txt 문법형식 변경
    backup.txt 마지막 행에 현재 차례를 나타냄

--- Othello/main.c
+++ Othello/main.c
@@ -2,13 +2,15 @@
 
 int g_map[10][10];
 int g_playtime;
-bool g_time_th_end = FALSE;  //  제한시간 스레드 종료 여부
-bool g_mouse_th_end = FALSE;  //  마우스 입력 스레드 종료 여부
-
+bool g_time_th_end = false;  //  제한시간 스레드 종료 여부
+bool g_mouse_th_end = false;  //  마우스 입력 스레드 종료 여부
+bool g_is_black_turn = true;
 void main()
 {
 	//  메모리 누수 체크 (디버그 모드)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	printf("hi");
 
+	init();
+	printMap();
+	printStone();
 }
